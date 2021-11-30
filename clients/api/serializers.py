18e8e9ffc121b98b4ababebbfe57f819e47d2ef8
RussianProgram <kurban.kerimhanov@gmail.com>
2021-11-30 15:51:28 +0300

    Добавил эндпоинт регистрации/логирования участника

--- clients/api/serializers.py
+++ clients/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from ..models import Client
-#from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 
 
 class ClientSerializer(serializers.ModelSerializer):
@@ -11,3 +11,27 @@ class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
         fields = ['user_id','user','email','first_name','last_name','photo','sex']
+
+class UserSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField()
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'password','password2', 'email', 'first_name', 'last_name']
+
+
+    def save(self, *args, **kwargs):
+        user = User(
+            email=self.validated_data['email'],
+            username=self.validated_data['username'],
+            first_name=self.validated_data['first_name'],
+            last_name=self.validated_data['last_name'],
+        )
+
+        password = self.validated_data['password']
+        password2 = self.validated_data['password2']
+
+        if password != password2:
+            raise serializers.ValidationError({password: "Пароль не совпадает"})
+        user.set_password(password)
+        user.save()
+        return user
